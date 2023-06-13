3c14ff9bc38cdbf3a597ed15e25073fd5c33c352
Nesah10101 <sdelosreyes0421@gmail.com>
2023-06-14 02:36:44 +0800

    Added sample HTML/CSS
    With errors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # 1 - create flask app
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # 2 - create route / this will stand as the url-extension (?)
 @app.route("/")
 # 3 - define the function under "/"
-def helloWorld():
-    return ("Hello, World!")
+def homePage():
+    return render_template('index.html')
 
 # para automatic mag run w/o 'flask run'
 if __name__ == '__main__':
